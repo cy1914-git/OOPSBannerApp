5ca99840c6170da585dc9d6c61a55716945018eb
Your Name <cy1914@srmist.edu.in>
2026-02-24 10:48:24 +0530

    chaithanya kishore added UC1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * 
+ * This class demostrata a simple java application that displays the Object
+ * Oriented Programming System OOPS acronym to the console.
+ * 
+ * @author chaithanya kishore
+ * @version 1.0
+ * 
+ */
+
+
+
+public class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println("OOPS");
+	}
+}
