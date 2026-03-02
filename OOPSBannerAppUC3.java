867aab68e43486915d9c736afeccbbb6ee4e31f1
YALAMAKURI CHAITHANYA KISHORE <cy1914@srmist.edu.in>
2026-03-02 09:18:31 +0530

    Add files via upload

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,17 @@
+/**
+ * @author chaithanya kishore
+ * @version 2.0
+ */
+public class OOPSBannerAppUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","  OOO    OOO   PPPP   SSSS  "));
+        System.out.println(String.join(" "," O   O  O   O  P   P S      "));
+        System.out.println(String.join(" "," O   O  O   O  P   P S      "));
+        System.out.println(String.join(" "," O   O  O   O  PPPP   SSS   "));
+        System.out.println(String.join(" "," O   O  O   O  P         S  "));
+        System.out.println(String.join(" "," O   O  O   O  P         S  "));
+        System.out.println(String.join(" ","  OOO    OOO   P      SSS   "));
+
+    }
+}
