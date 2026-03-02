59d1656e50474de626686b7c11030bba53310faa
YALAMAKURI CHAITHANYA KISHORE <cy1914@srmist.edu.in>
2026-03-02 09:25:02 +0530

    Add files via upload

---
+++ OOPSBannerAppUC5.java
@@ -0,0 +1,21 @@
+public class OOPSBannerAppUC5 {
+    public static void main(String[] args) {
+        
+        
+        String[] bannerLines = {
+            String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "PPPP ", " SS  "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ")
+        };
+        
+        
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+        
+    }
+}
